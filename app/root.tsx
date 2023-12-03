5b57b8c5b88c875645fbdbf19a0b893dd6488e61
Anderson Sousa <and3rsonsousa@outlook.com>
2023-12-03 17:06:47 -0300

    change

--- app/root.tsx
+++ app/root.tsx
@@ -26,8 +26,7 @@ export default function App() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K5J63GGC');</script>
-</body>`,
+})(window,document,'script','dataLayer','GTM-K5J63GGC')`,
 					}}
 				></script>
 				<meta charSet="utf-8" />
